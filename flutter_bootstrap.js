bebddad2b8c25c6d6dadbd3dfd4e0eac89d5be77
rassamaxaGT <rassamaxagt@gmail.com>
2025-09-08 11:38:02 +0300

    0809

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "537685472"
+    serviceWorkerVersion: "1780640271"
   }
 });
